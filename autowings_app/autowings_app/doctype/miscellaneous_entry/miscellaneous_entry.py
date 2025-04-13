43b6dd44791ad0a21cbd134e4d200815a56e4436
MD Faiyaz Ansari <faiyaz@adimyra.com>
2025-04-13 17:51:25 +0530

    miscellaneous update in sales invoice

---
+++ autowings_app/autowings_app/doctype/miscellaneous_entry/miscellaneous_entry.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Adimyra Systems Private Limited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class MiscellaneousEntry(Document):
+	pass
